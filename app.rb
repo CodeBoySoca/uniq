b93b211c8aa37c2c6a6d9a86fcfeebb4a6574146
KidMongoose <mongoose@kids-mbp.lan>
2022-12-02 14:52:44 -0800

    Added folder structure, templates and routes

---
+++ app.rb
@@ -0,0 +1,52 @@
+require 'rubygems'
+require 'sinatra'
+require 'haml'
+
+
+get '/' do
+  haml :index, :escape_html => false
+end
+
+get '/about' do
+  haml :about, :escape_html => false
+end
+
+get '/contact' do
+  haml :contact, :escape_html => false
+end
+
+get '/get-started' do
+  haml :'get-started', :escape_html => false
+end
+
+get '/listings' do
+  haml :listings, :escape_html => false
+end
+
+get '/listing' do
+  haml :listing, :escape_html => false
+end
+
+get '/loft' do
+  haml :loft, :escape_html => false
+end
+
+get '/sold-items' do
+  haml :'sold-items', :escape_html => false
+end
+
+get '/wishlist' do
+  haml :wishlist, :escape_html => false
+end
+
+get '/messages' do
+  haml :messages, escape_html => false
+end
+
+get '/profile' do
+  haml :profile, escape_html => false  
+end
+
+get '/add-items' do
+  haml :'add-items', escape_html => false
+end
